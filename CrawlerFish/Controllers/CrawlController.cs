35a808ff9a15090863be3b70d98e13d9e0e8ee30
Felipe Curvelo <felipe.curvelo@inspira.com.br>
2017-08-20 12:36:58 -0300

    Initial commit

---
+++ CrawlerFish/Controllers/CrawlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrawlerFish.Controllers
+{
+    public class CrawlController : ApiController
+    {
+		[HttpGet]
+		public HttpResponseMessage Get() {
+			return Request.CreateResponse(HttpStatusCode.OK, "Hi, I'm CrawlerFish!");
+		}
+    }
+}
---
+++ CrawlerFishTests/UnitTest/CrawlControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrawlerFish.Controllers;
+using System.Net;
+using Moq;
+using System.Net.Http;
+using System.Web.Http.Hosting;
+using System.Web.Http;
+
+namespace CrawlerFish.Tests {
+	[TestClass]
+	public class CrawlControllerTest {
+		[TestMethod]
+		public void TestMainAdressCall_ReturnHiMessage() {
+			var controller = new CrawlController() { Request = new HttpRequestMessage()	};
+			controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+			var response = controller.Get();
+
+			string actual = null;
+			response.TryGetContentValue(out actual);
+
+			Assert.AreEqual("Hi, I'm CrawlerFish!", actual);
+		}
+	}
+}
